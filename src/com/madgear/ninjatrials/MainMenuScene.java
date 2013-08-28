36b38d21faf7e148eab42b8c50e2dc8ccbc1917b
madwargo <lordwargo@gmail.com>
2013-08-28 12:42:10 +0200

    Added license to the new files.

--- src/com/madgear/ninjatrials/MainMenuScene.java
+++ src/com/madgear/ninjatrials/MainMenuScene.java
@@ -1,14 +1,31 @@
+/*
+ * Ninja Trials is an old school style Android Game developed for OUYA & using
+ * AndEngine. It features several minigames with simple gameplay.
+ * Copyright 2013 Mad Gear Games <madgeargames@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 package com.madgear.ninjatrials;
 
 import org.andengine.entity.Entity;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.EntityBackground;
-import org.andengine.entity.scene.background.RepeatingSpriteBackground;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.entity.text.Text;
 import org.andengine.entity.text.TextOptions;
 import org.andengine.util.adt.align.HorizontalAlign;
-import org.andengine.util.adt.color.Color;
 
 import com.madgear.ninjatrials.hud.SelectionStripe;
 
