83294091d2ba30fd090029e7ac7d057e5c47da44
madwargo <lordwargo@gmail.com>
2013-11-12 09:49:47 +0100

    Removed CharacterInfoScene.java class (already done in CharacterIntroScene.java). Added header in files SplashIntroScene and CharacterIntroScene.

--- src/com/madgear/ninjatrials/CharacterIntroScene.java
+++ src/com/madgear/ninjatrials/CharacterIntroScene.java
@@ -1,3 +1,22 @@
+/*
+ * Ninja Trials is an old school style Android Game developed for OUYA & using
+ * AndEngine. It features several minigames with simple gameplay.
+ * Copyright 2013 Mad Gear Games <madgeargames@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 package com.madgear.ninjatrials;
 
 import org.andengine.engine.handler.IUpdateHandler;
@@ -84,7 +103,10 @@ public class CharacterIntroScene extends GameScene{
 	}
 	
 	private void skip() {
-		SceneManager.getInstance().showScene(new TestingScene());
+        if(GameManager.DEBUG_MODE)
+            SceneManager.getInstance().showScene(new TestingScene());
+        else
+            SceneManager.getInstance().showScene(new RecordsScene());
 	}
 	
 	@Override
